5cc566073bddc431a412a12f77757a2509f24dc4
Jefferson Martínez Cuadros <jefferson.martinez@ADL27325.local>
2022-03-27 15:35:19 -0500

    advance since to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
+import Login from "./paginas/Login";
+import NuevoPassword from "./paginas/NuevoPassword";
+import OlvidePassword from "./paginas/OlvidePassword";
+import Registrar from "./paginas/Registrar";
 
+// console.log(import.meta.env.VITE_BACKEND_URL);
 
 function App() {
-
   return (
-    <div className="App">
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="olvide-password/:token" element={<NuevoPassword />} />
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
