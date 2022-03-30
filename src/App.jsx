25d0caaaa9d4cc0282000a608803395ee6d58ccc
Jefferson Martínez Cuadros <jefferson.martinez@ADL27325.local>
2022-03-29 19:47:22 -0500

    editar proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+import { ProyectosProvider } from "./context/ProyectosProvider";
 import AuthLayout from "./layouts/AuthLayout";
+import RutaProtegida from "./layouts/RutaProtegida";
 import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 import Login from "./paginas/Login";
 import NuevoPassword from "./paginas/NuevoPassword";
 import OlvidePassword from "./paginas/OlvidePassword";
+import NuevoProyecto from "./paginas/NuevoProyecto";
+import Proyectos from "./paginas/Proyectos";
 import Registrar from "./paginas/Registrar";
+import Proyecto from "./paginas/Proyecto";
+import EditarProyecto from "./paginas/EditarProyecto";
 
 // console.log(import.meta.env.VITE_BACKEND_URL);
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="registrar" element={<Registrar />} />
-          <Route path="olvide-password" element={<OlvidePassword />} />
-          <Route path="olvide-password/:token" element={<NuevoPassword />} />
-          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <ProyectosProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route
+                path="olvide-password/:token"
+                element={<NuevoPassword />}
+              />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
+
+            <Route path="/proyectos" element={<RutaProtegida />}>
+              <Route index element={<Proyectos />} />
+              <Route path="crear-proyecto" element={<NuevoProyecto />} />
+              <Route path=":id" element={<Proyecto />} />
+              <Route path="editar/:id" element={<EditarProyecto />} />
+            </Route>
+          </Routes>
+        </ProyectosProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
---
+++ src/context/ProyectosProvider.jsx
@@ -0,0 +1,129 @@
+import { useState, useEffect, createContext } from 'react';
+import clienteAxios from '../../config/clienteAxios';
+import { useNavigate } from 'react-router-dom';
+
+
+const ProyectosContext = createContext();
+
+const ProyectosProvider = ({ children }) => {
+    const [proyectos, setProyectos] = useState([]);
+    const [alerta, setAlerta] = useState({});
+    const [proyecto, setProyecto] = useState({});
+    const [cargando, setCargando] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const obtenerProyectos = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                if (!token) return
+    
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    }
+                }
+
+                const { data } = await clienteAxios('/proyectos', config);
+                setProyectos(data);
+    
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        obtenerProyectos();
+    }, []);
+
+    const mostrarAlerta = alerta => {
+        setAlerta(alerta)
+
+        setTimeout(() => {
+            setAlerta({})
+        }, 5000)
+    }
+
+    const submitProyecto = async proyecto => {
+        const { id } = proyecto;
+
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) return
+
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                }
+            }
+
+            const { data } = !!id ? await clienteAxios.put(`/proyectos/${proyecto.id}`, proyecto, config) : await clienteAxios.post('/proyectos', proyecto, config);
+            let messageAlerta = '';
+            // console.log(id);
+            if (!!id) {
+                // Sincronizar el state
+                const proyectosActualizados = proyectos.map(proyectoState => proyectoState._id === data._id ? data : proyectoState)
+                setProyectos(proyectosActualizados)
+                messageAlerta = 'Proyecto Actualizado correctamente';
+            } else {
+                setProyectos([...proyectos, data]);
+                messageAlerta = 'Proyecto Creado correctamente';
+            }
+            setAlerta({
+                msg: messageAlerta,
+                error: false,
+            })
+
+            setTimeout(() => {
+                setAlerta({})
+                navigate('/proyectos')
+            }, 3000)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const obtenerProyecto = async id => {
+        setCargando(true)
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) return
+
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                }
+            }
+
+            const { data } = await clienteAxios(`/proyectos/${id}`, config);
+            setProyecto(data);
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            setCargando(false);
+        }
+    }
+
+    return (
+        <ProyectosContext.Provider value={{
+            proyectos,
+            mostrarAlerta,
+            alerta,
+            submitProyecto,
+            obtenerProyecto,
+            proyecto,
+            cargando,
+        }}>
+            { children }
+        </ProyectosContext.Provider>
+    )
+}
+
+export {
+    ProyectosProvider
+}
+
+export default ProyectosContext;
