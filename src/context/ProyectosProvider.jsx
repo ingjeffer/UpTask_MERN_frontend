53e6ff98cb8a2ce7464a7f7d1ebdfa644bb7e4a5
Jefferson Martínez Cuadros <jefferson.martinez@ADL27325.local>
2022-03-29 19:54:39 -0500

    eliminar proyecto

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -107,6 +107,38 @@ const ProyectosProvider = ({ children }) => {
         }
     }
 
+    const eliminarProyecto = async id => {
+        try {
+            const token = localStorage.getItem('token')
+            if(!token) return
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await clienteAxios.delete(`/proyectos/${id}`, config)
+
+            // Sincronizar el state
+            const proyectosActualizados = proyectos.filter(proyectoState => proyectoState._id !== id )
+            setProyectos(proyectosActualizados)
+
+            setAlerta({
+                msg: data.msg,
+                error: false
+            })
+
+            setTimeout(() => {
+                setAlerta({})
+                navigate('/proyectos')
+            }, 3000);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <ProyectosContext.Provider value={{
             proyectos,
@@ -116,6 +148,7 @@ const ProyectosProvider = ({ children }) => {
             obtenerProyecto,
             proyecto,
             cargando,
+            eliminarProyecto,
         }}>
             { children }
         </ProyectosContext.Provider>
